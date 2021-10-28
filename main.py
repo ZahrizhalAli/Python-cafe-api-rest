9819b8a1dc333d5123c3fd28029b4cdbcb5d57d6
zahrizhalali29 <zzahrizhal.ali@gmail.com>
2021-10-28 22:24:19 +0800

    Finished

--- main.py
+++ main.py
@@ -71,11 +71,48 @@ def search_data():
         return jsonify(error={"Not Found": "Sorry, we can't find cafe at that location"})
 
 # HTTP POST - Create Record
-
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
 # HTTP PUT/PATCH - Update Record
+@app.route('/update-price/<cafe_id>', methods=['PATCH'])
+def update_price(cafe_id):
+    get_movie = Cafe.query.filter_by(id=cafe_id).first()
+    if get_movie:
+        get_movie.coffee_price = request.args.get('new_price')
+        db.session.commit()
+        return jsonify(result={"Message": "Successfuly updated!"})
+    else:
+        return jsonify(result={"Error": "Not updated."})
 
 # HTTP DELETE - Delete Record
-
+@app.route('/report-closed/<cafe_id>', methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key == "TopSecretAPIKey":
+        cafe_to_delete = Cafe.query.get(cafe_id)
+        if cafe_to_delete:
+            db.session.delete(cafe_to_delete)
+            db.session.commit()
+            return jsonify(success={"Message": "Berhasil dihapus"})
+        else:
+            return jsonify(error={"Message": "Cafe not found!"})
+    else:
+        return jsonify(error={"Error": "Wrong API Key"})
 
 if __name__ == '__main__':
     app.run(debug=True)
